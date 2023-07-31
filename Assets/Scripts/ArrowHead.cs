a5709d8c18025953a517e1ebe22eb8dacf15c3f8
bestdtang <94884915+MinPan9585@users.noreply.github.com>
2023-07-31 13:34:15 +0800

    course 0731

---
+++ Assets/Scripts/ArrowHead.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowHead : MonoBehaviour
+{
+    public GameObject deathvfx;
+    public AudioSource hitsfx;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            //play dead animation
+            //play vfx and sfx
+            hitsfx.Play();
+            Instantiate(deathvfx, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+        }
+    }
+}
